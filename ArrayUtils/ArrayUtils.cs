a0b96797aaeb03f89c1a7c0f66600caf46d25ba4
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2020-10-26 11:00:39 +0200

    [NEW EXAMPLE] Added demo for arrays

---
+++ ArrayUtils/ArrayUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ArrayUtils
+{
+    public static class CustomArrayUtils
+    {
+        public static double[] CreateArrayFromTemplate(double startX, double dx, int size, Func<double, double> func)
+        {
+            double[] data = new double[size];
+            double x = startX;
+            for (int i = 0; i < size; ++i)
+            {
+                data[i] = func(x);
+                x += dx;
+            }
+            return data;
+        }
+
+        public static void PrintArray(double[] data)
+        {
+            for (int i = data.GetLowerBound(0); i <= data.GetUpperBound(0); ++i)
+            {
+                Console.WriteLine("arr[{0}]={1:#0.####}", i, data[i]);
+            }
+        }
+
+        public static void ProcessArray(double[] data, out double aMin, out double aMax, out double aAvg)
+        {
+            double S = data[0];
+            aMin = aMax = S;
+            for (int i = data.GetLowerBound(0) + 1; i <= data.GetUpperBound(0); ++i)
+            {
+                S += data[i];
+                
+                if (aMin > data[i])
+                {
+                    aMin = data[i];
+                }
+
+                if (aMax < data[i])
+                {
+                    aMax = data[i];
+                }
+            }
+
+            aAvg = S / data.Length;
+        }
+
+        public static int PostProcessArray(double[] data, Predicate<double> pred)
+        {
+            int count = 0;
+            for (int i = data.GetLowerBound(0); i <= data.GetUpperBound(0); ++i)
+            {
+                if (pred(data[i]))
+                {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ ArraysApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ArrayUtils;
+
+namespace ArraysApp
+{
+    class Program
+    {
+        static double Func2(double x1, double x2)
+        {
+            return Math.Abs(Math.Cos(Math.Exp(x2 / x1)))/(4 + x2);
+        }
+
+        static void Main(string[] args)
+        {
+            double startX = 1, dx = 0.5;
+            const int size = 10;
+            double[] arr = CustomArrayUtils.CreateArrayFromTemplate(startX, dx, size, x => Func2(x * 2.76, x * 0.5));
+            CustomArrayUtils.PrintArray(arr);
+            
+            CustomArrayUtils.ProcessArray(arr, out double min, out double max, out double avg);
+            
+            Console.WriteLine("Min={0:#0.####}, Max={1:#0.####}, Avg={2:#0.####}", min, max, avg);
+
+            int count = CustomArrayUtils.PostProcessArray(arr, x => x >= 0.9 * avg && x <= 1.1 * avg);
+            Console.WriteLine("R={0}", count);
+            
+        }
+    }
+}
